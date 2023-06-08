5e2ed5f9f1acc914832c02e5e903ea6c4424dd18
Sudongk <aa88sd@naver.com>
2023-06-08 20:20:25 +0900

    init v3

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,5 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
-        String url = "jdbc:mysql://localhost:3306/testDB" +
-                "?serverTimezone=Asia/Seoul&characterEncoding=UTF-8";
-
-            Connection conn;
-
-            try {
-                conn = DriverManager
-                        .getConnection(url, "root", "rlatnehd@123");
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-
-        System.out.println("connection success");
     }
 }
