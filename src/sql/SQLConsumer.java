b12ae452eda3c02f9fe33d6b31c4d118dbb5b203
Sudongk <aa88sd@naver.com>
2023-06-12 15:52:34 +0900

    sync with origin

--- src/sql/SQLConsumer.java
+++ src/sql/SQLConsumer.java
@@ -1,8 +1,8 @@
-//package sql;
-//
-//import java.sql.SQLException;
-//
-//@FunctionalInterface
-//public interface SQLConsumer<T> {
-//    void accept(T t) throws SQLException;
-//}
+package sql;
+
+import java.sql.SQLException;
+
+@FunctionalInterface
+public interface SQLConsumer<T> {
+    void accept(T t) throws SQLException;
+}
