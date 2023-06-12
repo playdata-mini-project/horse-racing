80dc2ddcf2128e752b975758574c8fecbb8cbf58
Sudongk <aa88sd@naver.com>
2023-06-12 15:45:29 +0900

    solve conflict

---
+++ src/config/jdbc/JdbcConnection.java
@@ -0,0 +1,22 @@
+package config.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class JdbcConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/horse_racing_db"
+            + "?serverTimezone=Asia/Seoul&characterEncoding=UTF-8";
+    private static final String USER = "root";
+    private static final String PASSWORD = "1234";
+    public static Connection getConnection(){
+        Connection conn;
+
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return conn;
+    }
+}
